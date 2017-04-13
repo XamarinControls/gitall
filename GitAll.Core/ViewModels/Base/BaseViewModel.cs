a4803c7e810d92333a084e88a8d9bcc1cd083050
Jose Manuel Montero <jmmontero.ortega@gmail.com>
2017-04-13 08:16:38 +0200

    Include base View model

---
+++ GitAll.Core/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GitAll.Core.ViewModels.Base
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string property = null)
+        {
+            var args = new PropertyChangedEventArgs(property);
+            PropertyChanged?.Invoke(this, args);
+        }
+        
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string property = null)
+        {
+            if(EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            RaisePropertyChanged(property);
+            return true;
+        }
+    }
+}
