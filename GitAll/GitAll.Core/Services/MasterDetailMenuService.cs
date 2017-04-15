3f84941015b2c7bb434a43baa48afbbeb6532f93
Jose Manuel Montero <jmmontero.ortega@gmail.com>
2017-04-15 13:18:54 +0200

    Include a MasterDetail service

---
+++ GitAll/GitAll.Core/Services/MasterDetailMenuService.cs
@@ -0,0 +1,17 @@
+﻿using GitAll.Core.Services.Interfaces;
+using System.Collections.Generic;
+using GitAll.Core.Model;
+
+namespace GitAll.Core.Services
+{
+    public class MasterDetailMenuService : IMasterDetailMenuService
+    {
+        public List<MasterItemDetail> MenuOptions()
+        {
+            var options = new List<MasterItemDetail>();
+
+
+            return options;
+        }
+    }
+}
